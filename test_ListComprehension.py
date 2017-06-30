4972cbe567c003569ca8703ff341941497ca9cb6
Pan Yan Ru <q0u04bj3@gmail.com>
2017-06-30 09:54:47 +0800

    Add files via upload

---
+++ test_ListComprehension.py
@@ -0,0 +1,10 @@
+import unittest
+from ListComprehension import fun
+
+class PerfectNumTest(unittest.TestCase):
+    
+    def test_fun(self):
+        self.assertEqual(fun(1,11), -55)
+
+if __name__ == '__main__':
+    unittest.main()
