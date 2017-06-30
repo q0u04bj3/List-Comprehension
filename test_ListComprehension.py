90838ee8cf6ef47dae6af2215d3929477b5b5e07
Pan Yan Ru <q0u04bj3@gmail.com>
2017-06-30 15:41:59 +0800

    Update test_ListComprehension.py

--- test_ListComprehension.py
+++ test_ListComprehension.py
@@ -3,8 +3,36 @@ from ListComprehension import fun
 
 class ListComprehensionTest(unittest.TestCase):
     
-    def test_fun(self):
-        self.assertEqual(fun(1,11), -55)
+    def test_ListComprehension_zero(self):
+        expected_value = 0
+        value = fun(0,0)
+        self.assertEqual(expected_value, value)
+    
+    def test_ListComprehension_num1(self):
+        expected_value = -55
+        value = fun(1,11)
+        self.assertEqual(expected_value, value)
+    
+    def test_ListComprehension_num1(self):
+        expected_value = 4951
+        value = fun(1,20)
+        self.assertEqual(expected_value, value)
+
+    def test_ListComprehension_num2(self):
+        expected_value = 0
+        value = fun(10,10)
+        self.assertEqual(expected_value, value)
+
+    def test_ListComprehension_num3(self):
+        expected_value = 0
+        value = fun(-1,100)
+        self.assertEqual(expected_value, value)
+
+    def test_ListComprehension_num4(self):
+        expected_value = 0
+        value = fun(-1,-100)
+        self.assertEqual(expected_value, value)
+
 
 if __name__ == '__main__':
     unittest.main()
